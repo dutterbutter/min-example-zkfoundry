ebb8f3b62bf7ca6ab4a7c2751fd46e3966f10080
Dustin Brickwood <dustinbrickwood204@gmail.com>
2024-01-11 12:35:11 -0600

    chore: update nft script

--- script/NFT.s.sol
+++ script/NFT.s.sol
@@ -6,21 +6,11 @@ import "../src/NFT.sol";
 
 contract MyScript is Script {
     function run() external {
-        // Simulate a transaction from a specific address with the required ETH value
-        // Address used here is from era-test-node rich wallets
-        address someAddress = address(0x42F3dc38Da81e984B92A95CBdAAA5fA2bd5cb1Ba);
-        uint256 mintPrice = 0.08 ether;
+        uint256 privateKey = uint256(bytes32(bytes20(keccak256(abi.encodePacked("PRIVATE-KEY")))));
+        vm.startBroadcast(privateKey);
 
-        vm.deal(someAddress, mintPrice);
-        vm.prank(someAddress);
-
-        // Deploy the NFT contract
         NFT nft = new NFT("NFT_tutorial", "TUT", "baseUri");
-
-        // Mint an NFT to someAddress
-        vm.prank(someAddress);
-        uint256 tokenId = nft.mintTo{value: mintPrice}(someAddress);
-        // Should be viewable in era-test-node
-        console.log("NFT Contract deployed and token minted. Token ID:", tokenId);
+        
+        vm.stopBroadcast();
     }
 }
