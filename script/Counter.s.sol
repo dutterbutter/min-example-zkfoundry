30f8a4303d30db0d01266d6a28521526ac9c3648
Dustin Brickwood <dustinbrickwood204@gmail.com>
2024-02-10 13:00:49 -0600

    chore: update for zkquest

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,25 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import "forge-std/Script.sol";
-import "../src/Counter.sol";
+import {Script, console2} from "forge-std/Script.sol";
 
 contract CounterScript is Script {
-    Counter counter;
-
-    function setUp() public {
-        // Deploy the Counter contract
-        counter = new Counter();
-    }
+    function setUp() public {}
 
     function run() public {
-        // Set the number to a specific value
-        uint256 initialValue = 42;
-        vm.prank(address(this));
-        counter.setNumber(initialValue);
-
-        // Increment the number
-        vm.prank(address(this));
-        counter.increment();
+        vm.broadcast();
     }
 }
